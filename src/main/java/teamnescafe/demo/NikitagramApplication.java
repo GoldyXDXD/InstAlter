fef0c798ad4e24323c506b7ebca81fb3f27ba4fd
GoldyXDXD <62290862+GoldyXDXD@users.noreply.github.com>
2022-03-17 20:35:40 +0300

    .

---
+++ src/main/java/teamnescafe/demo/NikitagramApplication.java
@@ -0,0 +1,13 @@
+package teamnescafe.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NikitagramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikitagramApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teamnescafe/demo/NikitagramApplicationTests.java
@@ -0,0 +1,13 @@
+package teamnescafe.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NikitagramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
